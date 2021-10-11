60260d339a12279fa3b9c5a1fc2b81b53de8013f
Miguel <miguelwork92@gmail.com>
2021-10-11 21:14:36 +0200

    v1.0.1: remove js from repo and ts from npm

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-declare type ReentrantLockReleaser = () => void;
-export declare class ReentrantLock {
-    private current?;
-    private last?;
-    private next;
-    acquire(): Promise<ReentrantLockReleaser>;
-    lock<T>(op: () => Promise<T>): Promise<T>;
-}
-export {};
